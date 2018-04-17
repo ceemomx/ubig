8ce212a54ff7603b6acda97d35ff5312d9e44bc4
zhaomen <iloveloony@163.com>
2018-04-17 18:14:05 +0800

    1

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const http = require('http');
-const path = require('path');
-const staticDir = path.join(__dirname, 'public');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const staticDir = path.join(__dirname, 'public')
+const formidable = require('formidable')
 let router = require('./lib/router')
 
-router.setStatic(staticDir);
+router.setStatic(staticDir)
 
 router.get('/info', (req, res) => {
     res.end('555')
 })
 
-http.createServer(router.router()).listen(4000);
+router.post('/admin/up/photos', (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.on('file', function(name, file) {
+        fs.copyFileSync(file.path, path.join(__dirname,'source', file.name))
+        fs.unlinkSync(file.path)
+    })
+    form.parse(req, (err, feilds, files) => {
+        res.end('parse end')
+    })
+
+})
+
+http.createServer(router.router()).listen(4000)
